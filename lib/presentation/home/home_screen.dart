7ae04feb3dbc469adf497e1cd4c6ea5f5de3208e
Vyshnav_Vijayan <vvijayan854@gmail.com>
2023-12-22 00:02:38 +0530

    ok

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -58,7 +58,7 @@ class HomeScreen extends StatelessWidget {
                     },
                   );
                 },
-              // ),
+              ),
             ),
           ],
         ),
